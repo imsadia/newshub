7877046c43247dc6b21f2aa82c7dc441066ec21f
im-sadiamalik <115077666+im-sadiamalik@users.noreply.github.com>
2023-02-06 12:39:28 +0500

    Top loading bar reactjs

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import NewsItem from './NewsItem'
+import LoadingBar from 'react-top-loading-bar'
 
 function News(props) {
     // Initialize all state variables
@@ -7,6 +8,7 @@ function News(props) {
     const [totalResults, setTotalResults] = useState(0);
     const [loading, setLoading] = useState(true);
     const [page, setPage] = useState(1);
+    const [progress, setProgress] = useState(10);
 
     // Function to fetch news in the begining
     const fetchNews = async () => {
@@ -15,13 +17,17 @@ function News(props) {
             : `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&page=${page}&pageSize=${props.pageSize}&apiKey=${props.apiKey}`;
 
         let response = await fetch(url);
+        setProgress(50);
+
         let parsedRespones = await response.json();
         let articles = await parsedRespones.articles;
+        setProgress(70);
 
         // Set state variables
         setArticles(articles);
         setTotalResults(parsedRespones.totalResults);
         setLoading(false);
+        setProgress(100);
 
         console.log(parsedRespones.articles);
         console.log(articles);
@@ -58,10 +64,17 @@ function News(props) {
         <div className="container my-4">
             <h1 className="text-center my-5">NewsHub - {capitalizeFirstLetter(props.category)}</h1>
 
+            <LoadingBar
+                height={3}
+                color='#FF6A3D'
+                progress={progress}
+                onLoaderFinished={() => setProgress(0)}
+            />
+
             <div className="row">
                 {!loading && articles.map((element) => {
-                    return <div className="col-lg-4 col-md-4 col-sm-6 my-3" key={element.url}>
-                        <NewsItem title={element.title} description={element.description} link={element.url} imageUrl={element.urlToImage} />
+                    return <div className="col-lg-4 col-md-4 col-sm-6 my-3 d-flex align-items-stretch" key={element.url}>
+                        <NewsItem title={element.title} description={element.description} link={element.url} imageUrl={element.urlToImage} author={element.author} publishedAt={element.publishedAt} />
                     </div>
                 })}
             </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import moment from 'moment';
 
 function NewsItem(props) {
     const defaultImageUrl = 'https://media.cnn.com/api/v1/images/stellar/prod/230127123847-olympic-rings-paris.jpg?c=16x9&q=w_800,c_fill';
-    let { title, description, link, imageUrl } = props;
+    let { title, description, link, imageUrl, author, publishedAt } = props;
 
     return (
         <div className="card">
@@ -10,9 +11,10 @@ function NewsItem(props) {
             <div className="card-body">
                 <h5 className="card-title">{title ? title.slice(0, 50) : description.slice(0, 50)}...</h5>
                 <p className="card-text">{description ? description.slice(0, 100) : title}...</p>
+                <a href={link} className="btn btn-sm">Read More &#187;</a>
             </div>
-            <div className="card-body">
-                <a href={link} className="btn btn-dark btn-sm">Read More &#187;</a>
+            <div className="card-footer">
+                <small style={{ float: 'right', color: '#1A2238' }}>Published by <strong>{author ? author : "Unknown"}</strong> - <strong  className='text-danger'>{moment(publishedAt).fromNow()}</strong></small>
             </div>
         </div>
     )
